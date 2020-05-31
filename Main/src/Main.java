66d7b5d2565db4728efc6d93c9d90c0b6a0041c4
Hüseyin Gülçiçek <33606081+huseyingulcicek08@users.noreply.github.com>
2020-05-31 18:25:31 +0300

    Add files via upload

---
+++ Main/src/Main.java
@@ -0,0 +1,104 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.util.Random;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main extends JPanel {
+
+    private static final long serialVersionUID = 4190408071671335505L; // to get rid of the warning in eclipse
+    private static final int frameX = 1280, frameY = 720; // width and height for the JFrame
+    private static final String title = "Galton Board Simulation"; // title for the JFrame
+    private int n, nTodo; // number of balls and balls left to be drawn
+    private static boolean simRunning; // true as long as nTodo > 0
+    private Random rand; // to generate random numbers
+    private int[] columns; // to store the number of balls in each column
+
+    public Main(int n, int columns) {
+        this.n = n; // total number of balls to place
+        nTodo = n; // number of balls to place starts at total
+        simRunning = true; // when simulation starts
+        this.columns = new int[columns]; // initialize columns array to the specified number of columns
+        rand = new Random(); // to generate random numbers
+    }
+
+    public void placeBall() { // places a ball
+        int bin = 0, choice;
+        for (int i=0; i<columns.length; i++) { // performs choose operation #columns times
+            choice = rand.nextInt(2); // generate random 0 or 1
+            if (choice==1 && bin < columns.length-1) // if 1, move 1 to the right
+                bin+= 1;
+            // if 0, keep ball in same place
+        }
+        columns[bin] += 1; // place ball in the column we found
+        nTodo-=1; // 1 less ball to place
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        // draw background
+        g2d.setColor(Color.black);
+        g2d.fillRect(0,  0, frameX, frameY);
+        // draw columns
+        for(int i=0; i<columns.length; i++) {
+            g2d.setColor(Color.white);
+            g2d.drawRect(i + i*(frameX/columns.length) - 10, 0, frameX/columns.length, frameY); // draw outlines of columns
+            g2d.fillRect(i + i*(frameX/columns.length) - 10, 0, frameX/columns.length, (frameY - 35) - columns[i]/(3*(n/10000)*(int)(Math.log10(columns.length))+1)); // draws columns in reverse
+        }
+        // draw counter
+        g2d.setColor(Color.black);
+        g2d.drawString("# of balls drawn: ", frameX - 150, 20);
+        g2d.drawString("" + (n - nTodo) + "/" + n, frameX - 150, 40);
+    }
+
+    public static void skipLine() {
+        for (int i=0; i<50; i++)
+            System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int input = 1;
+        JFrame frame = new JFrame(title); // create new frame with title
+        Main simulation;
+
+        frame.setSize(frameX, frameY); // choose frame size
+        frame.setResizable(false); // fixed frame size
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // frame terminates on close
+        frame.setVisible(false);
+
+        skipLine();
+        while(input > 0) { // loops forever, until window is closed
+            System.out.print("Enter # of balls: "); // prompt the user to enter the number of balls to be dropped
+            input = in.nextInt(); // stores next integer entered by the user
+            if (input < 1) // if invalid input, exit
+                continue; // skips everything below this until the loop condition
+            simulation = new Main(input, 25); // create instance of simulation (number of balls, number of columns)
+            frame.add(simulation); //add instance of simulation to frame
+            frame.setFocusable(true); // make frame main focus
+            frame.setVisible(true); // frame appears on screen
+            frame.setAlwaysOnTop(true); // shows frame always on top
+
+            long runtime = System.nanoTime(); // get the run times
+            System.out.print("Simulation running ..."); // tell the user that the simulation is starting
+            while(simRunning) { // run simulation as long as simRunning is true
+                simulation.placeBall(); // places a single ball on the board
+                simulation.repaint(); // repaints the board
+                if (simulation.nTodo <= 0) // if there are no more balls to place
+                    simRunning = false; // change simRunning to false, loop will exit on next iteration
+            }
+            runtime = System.nanoTime() - runtime; // get runtime by subtracting current time - previously saved time
+            System.out.println(" done. \n" + "Runtime: " + (runtime/1000000) + "ms \n\n"); // tell the user simulation is done and output runtime
+        }
+        System.out.println("Exiting...");
+        in.close();
+        System.exit(0);
+    }
+}
